52b2425c74ecbf3392b56610eceac03722f54a3b
Aayodhya Satale <sataleaayushri@gmail.com>
2025-06-15 14:28:16 +0530

    updated

--- src/test/java/com/test/CalcTest.java
+++ src/test/java/com/test/CalcTest.java
@@ -1,25 +1,11 @@
 package com.test;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import junit.framework.Assert;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
  
 public class CalcTest {
- 
-    @BeforeClass
-    public void setUp() throws Exception {
- 
-    }
- 
-    @AfterClass
-    public void tearDown() throws Exception {
- 
-    }
  
     @Test
     public void CanAddNumbers()
@@ -28,39 +14,5 @@ public class CalcTest {
     	Assert.assertEquals(x, 5);
 
     }
- 
- 
-//    @Test
-//    public void CanSubtract()
-//    {
-//       assertThat(Calculator.Subtract(1, 1), is(0));
-//        assertThat(Calculator.Subtract(-1, -1), is(0));
-//        assertThat(Calculator.Subtract(100, 5), is(95));
-//    }
-// 
-// 
-//    @Test
-//    public void CanMultiply()
-//    {
-//        assertThat(Calculator.Multiply(1, 1), is(1));
-//        assertThat(Calculator.Multiply(-1, -1), is(1));
-//        assertThat(Calculator.Multiply(100, 5), is(500));
-//    }
-// 
-// 
-//    public void CanDivide()
-//    {
-//        assertThat(Calculator.Divide(1, 1), is(1));
-//        assertThat(Calculator.Divide(-1, -1), is(1));
-//        assertThat(Calculator.Divide(100, 5), is(20));
-//    }
-// 
-// 
-//    @Test
-//    public void CanDoStuff()
-//    {
-//        assertThat(true, is(true));
-//    }
-// 
  
 }
